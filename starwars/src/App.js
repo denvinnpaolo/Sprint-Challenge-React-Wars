39c93d11ef0e4972efee6a9e72a3d5de549e20ee
Denvinn Magsino <denvinnmagsino@Denvinns-MacBook-Air.local>
2019-09-09 17:51:54 -0700

    just incase I didnt push the finsihed version

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+  
+import React from 'react';
 import axios from "axios";
-import { Button, Header, Icon, Image, Modal } from "semantic-ui-react";
 import './App.css';
-import StarWarsCard from"./components/StarWarsCard" ;
+import StarWarsBase from "./components/StarWarsBase";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,46 +11,11 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
-  const ModalExampleTopAligned = () => (
-    <Modal trigger={<Button>Show Modal</Button>} centered={false}>
-      <Modal.Header>Select a Photo</Modal.Header>
-      <Modal.Content image>
-        <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
-        <Modal.Description>
-          <Header>Default Profile Image</Header>
-          <p>We've found the following gravatar image associated with your e-mail address.</p>
-          <p>Is it okay to use this photo?</p>
-        </Modal.Description>
-      </Modal.Content>
-    </Modal>
-  )
-  const [character, setCharacter] = useState([]);
-
-  useEffect(()=>{
-    setCharacter([]);
-
-    axios
-        .get(`https://swapi.co/api/people/`)
-        .then(result=>{
-            console.log(result.data.results);
-            setCharacter(result.data.results);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, []);
-
-
   return (
-    <div className="App">{
-      <h1 className="Header">React Wars</h1>,
-      character.map((character, index)=>{
-        console.log(character)
-        return <StarWarsCard character={character} key={index}/>
-      })
-
-    }
+    <div className="App">
+      <h1 className="Header">React Wars</h1>
+      <h2>by Denvinn Magsino</h2>
+      <StarWarsBase />
     </div>
   );
 }
